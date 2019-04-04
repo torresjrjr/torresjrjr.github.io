8eac769a935a7f47dc96137f6aaac60a77fe68ba
torresjrjr <43079131+torresjrjr@users.noreply.github.com>
2019-04-04 22:01:07 +0100

    futher hard styles; and js

---
+++ assets/js/layout_default.js
@@ -0,0 +1,12 @@
+function lightmode() {
+	document.getElementById("big").style.backgroundColor = "#FFFFFF";
+	document.getElementById("main").style.fontSize = "";
+	document.getElementById("main").style.color = "#000000";
+	document.getElementById("main").style.backgroundColor = "#DDDDDD";
+}
+function darkmode() {
+	document.getElementById("big").style.backgroundColor = "#000000";
+	document.getElementById("main").style.fontSize = "";
+	document.getElementById("main").style.color = "#999988";
+	document.getElementById("main").style.backgroundColor = "#000909";
+}
