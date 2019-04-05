9351e09a22d8a05f3ed4882f71203b1b9d693d0d
torresjrjr <43079131+torresjrjr@users.noreply.github.com>
2019-04-05 19:46:08 +0100

    experiementing with cookies

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,8 @@ function darkmode() {
 	document.getElementById("main").style.color = "#999988";
 	document.getElementById("main").style.backgroundColor = "#000909";
 }
+
+// Attempt at using a cookie //
+// example to help:
+// document.cookie = "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
+document.cookie = "viewmode=dark_mode_1; expires=1 Jan 9999 12:00:00 UTC;";
