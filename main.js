168631c848663ea0b0a907542cb4b9ec85de3c8a
torresjrjr <43079131+torresjrjr@users.noreply.github.com>
2019-04-05 22:04:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function lightmode() {
 	document.getElementById("main").style.color = "#000000";
 	document.getElementById("main").style.backgroundColor = "#DDDDDD";
 	document.cookie = "viewmode=light_mode_1; expires=1 Jan 9999 12:00:00 UTC;";
-	console.log("main.js; cookie: viewmode=light_mode_1")
+	console.log("main.js; lightmode(); cookie: viewmode=light_mode_1")
 }
 function darkmode() {
 	document.getElementById("big").style.backgroundColor = "#000000";
@@ -13,7 +13,7 @@ function darkmode() {
 	document.getElementById("main").style.color = "#999988";
 	document.getElementById("main").style.backgroundColor = "#000909";
 	document.cookie = "viewmode=dark_mode_1; expires=1 Jan 9999 12:00:00 UTC;";
-	console.log("main.js; cookie: viewmode=dark_mode_1")
+	console.log("main.js; darkmode(); cookie: viewmode=dark_mode_1")
 }
 
 console.log("main.js; cookie reader script start")
@@ -38,11 +38,11 @@ function checkCookie() {
   var viewmode=getCookie("viewmode");
   if (viewmode = "light_mode_1") {
     lightmode()
-    console.log("checkCookie : light_mode_1")
+    console.log("main.js; checkCookie(); light_mode_1")
   } 
   if (viewmode = "dark_mode_1") {} {
   	darkmode()
-  	console.log("checkCookie : dark_mode_1")
+  	console.log("main.js; checkCookie(); dark_mode_1")
   }
 }
 
