76fac303802fd6f126bad01f8030974ff67952de
torresjrjr <43079131+torresjrjr@users.noreply.github.com>
2019-04-06 02:58:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -52,21 +52,30 @@ console.log("main.js; cookie reader script end")
 // Blog search bar. //
 <script>
 function BlogSearchbar() {
+  console.log("BlogSearchbar")
   // Declare variables
   var input, filter, ul, li, a, i;
   input = document.getElementById("blog-searchbar");
+  console.log(input);
   filter = input.value.toUpperCase();
+  console.log(filter);
   ul = document.getElementById("blog-list-posts");
+  console.log(ul);
   li = ul.getElementsByTagName("li");
+  console.log(li);
 
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("a")[0];
+    console.log(a)
     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
+      console.log("if is true")
     } else {
       li[i].style.display = "none";
+      console.log("else is true")
     }
   }
+  console.log("BlogSearchbar END")
 }
 </script>
