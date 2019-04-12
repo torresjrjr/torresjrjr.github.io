91cf5bd0e7047fa8838492bc32097307969d0b5e
torresjrjr <43079131+torresjrjr@users.noreply.github.com>
2019-04-12 16:08:51 +0100

    + MD Editor page

--- main.js
+++ main.js
@@ -84,3 +84,50 @@ function BlogSearchbar() {
   console.log("BlogSearchbar END")
 }
 // END Blog search bar. //
+
+//--- MD Editor. ---//
+// From https://github.com/benweet/stackedit.js/blob/master/docs/index.md //
+const el = document.querySelector('textarea');
+const stackedit = new Stackedit();
+
+// Open the iframe
+stackedit.openFile({
+  name: 'Filename', // with an optional filename
+  content: {
+    text: el.value // and the Markdown content.
+  }
+});
+
+// Listen to StackEdit events and apply the changes to the textarea.
+stackedit.on('fileChange', (file) => {
+  el.value = file.content.text;
+});
+
+// --- //
+// Conversion service
+stackedit.openFile({
+  name: 'Filename',
+  content: { text: 'Hello **Markdown** writer!' }
+}, true /* silent mode */);
+
+// In silent mode, the `fileChange` event is emitted only once.
+stackedit.on('fileChange', (file) => {
+  divEl.innerHTML = file.content.html;
+});
+/*/
+// --- //
+/* Custom Properties
+stackedit.openFile({
+  name: 'Filename',
+  content: {
+    text: 'Hello **CommonMark** writer!',
+    properties: {
+      extensions: {
+        preset: 'commonmark'
+      }
+    }
+  }
+});
+/*/
+
+// END MD Editor. //
