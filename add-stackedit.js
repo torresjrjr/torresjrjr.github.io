cb8745d1857239eafd588489dc3c0862e66f3d6f
torresjrjr <43079131+torresjrjr@users.noreply.github.com>
2019-04-15 21:56:26 +0100

    Update add-stackedit.js

--- add-stackedit.js
+++ add-stackedit.js
@@ -217,24 +217,11 @@ const stackedit = new Stackedit();
 
 // Open the iframe
 stackedit.openFile({
-  name: 'Filename', // with an optional filename
-  content: {
-    text: el.value // and the Markdown content.
-  }
-});
-
-// Listen to StackEdit events and apply the changes to the textarea.
-stackedit.on('fileChange', (file) => {
-  el.value = file.content.text;
-});
-
-// Conversion Service.
-stackedit.openFile({
   name: 'Filename',
   content: { text: 'Hello **Markdown** writer!' }
 }, true /* silent mode */);
 
-// In silent mode, the `fileChange` event is emitted only once.
+// Listen to StackEdit events and apply the changes to the textarea.
 stackedit.on('fileChange', (file) => {
-  divEl.innerHTML = file.content.html;
+  el.value = file.content.text;
 });
