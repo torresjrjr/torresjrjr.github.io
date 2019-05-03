ffef77ec2398d2a6d32d52e28ae815f5925de2c1
torresjrjr <43079131+torresjrjr@users.noreply.github.com>
2019-05-04 00:27:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 
 // viewmode. //
 function viewmode_light_mode_1() {
+	console.log("main.js; viewmode_light_mode_1(); cookie: {viewmode=light_mode_1; expires=1 Jan 9999 12:00:00 UTC; path=/;}");
 	document.getElementById("big").style.backgroundColor = "#DDDDDD";
 	document.getElementById("main").style.fontSize = "";
 	document.getElementById("main").style.color = "#000000";
 	document.getElementById("main").style.backgroundColor = "#EEEEEE";
 	document.cookie = "viewmode=light_mode_1; expires=1 Jan 9999 12:00:00 UTC; path=/;";
-	console.log("main.js; viewmode_light_mode_1(); cookie: {viewmode=light_mode_1; expires=1 Jan 9999 12:00:00 UTC; path=/;}");
 }
 function viewmode_dark_mode_1() {
+	console.log("main.js; viewmode_dark_mode_1(); cookie: {viewmode=dark_mode_1; expires=1 Jan 9999 12:00:00 UTC; path=/;}");
 	document.getElementById("big").style.backgroundColor = "#000000";
 	document.getElementById("main").style.fontSize = "";
 	document.getElementById("main").style.color = "#999988";
 	document.getElementById("main").style.backgroundColor = "#000909";
 	document.cookie = "viewmode=dark_mode_1; expires=1 Jan 9999 12:00:00 UTC; path=/;";
-	console.log("main.js; viewmode_dark_mode_1(); cookie: {viewmode=dark_mode_1; expires=1 Jan 9999 12:00:00 UTC; path=/;}");
 }
 
 function setViewmode(viewmode) {
+console.log("main.js; setViewmode(viewmode);");
   if (viewmode == "light_mode_1") {
-    viewmode_light_mode_1();
     console.log("main.js; setViewmode(light_mode_1); viewmode_light_mode_1()");
+    viewmode_light_mode_1();
   } 
   if (viewmode == "dark_mode_1") {
-    viewmode_dark_mode_1();
     console.log("main.js; setViewmode(dark_mode_1); viewmode_dark_mode_1()");
+    viewmode_dark_mode_1();
   }
 }
 
@@ -46,9 +47,9 @@ function getCookie(cname) {
 }
 
 function checkCookie() {
+  console.log("main.js; checkCookie(); setViewmode(viewmode); viewmode = ...")
   var viewmode = getCookie("viewmode");
   setViewmode(viewmode);
-  console.log("main.js; checkCookie(); setViewmode(viewmode); viewmode = ", viewmode)
 }
 // END viewmode. //
 
