5cafad818ae23d9436d69a57897fff823f8db716
Byron Torres <43079131+torresjrjr@users.noreply.github.com>
2019-09-27 23:09:25 +0100

    Create df.sh

---
+++ df.sh
@@ -0,0 +1,9 @@
+#
+# @torresjrjr
+# df.sh , dotfiles.
+# Run this file at the home directory.
+
+# .vimrc
+curl https://gist.githubusercontent.com/torresjrjr/818575d422b17b6daab2e7ec2d7f8e23/raw/650834cce4a3af9bd14620b6f6b91290e32b78ae/.vimrc > .vimrc
+
+#
