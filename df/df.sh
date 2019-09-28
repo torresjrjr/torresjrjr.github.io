3dc2c4e67cde3041e0b695eb5f123dd9dc9859af
Byron Torres <43079131+torresjrjr@users.noreply.github.com>
2019-09-28 19:17:51 +0100

    Update and rename df.sh to df/df.sh

--- df/df.sh
+++ df/df.sh
@@ -1,4 +1,5 @@
-#
+
+# ::: df.sh :::
 # @torresjrjr
 # df.sh , dotfiles.
 # Run this file at the home directory.
@@ -6,4 +7,4 @@
 # .vimrc
 curl https://gist.githubusercontent.com/torresjrjr/818575d422b17b6daab2e7ec2d7f8e23/raw/650834cce4a3af9bd14620b6f6b91290e32b78ae/.vimrc > .vimrc
 
-#
+# END
